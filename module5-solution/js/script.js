8fccd48af54eac61439adaf598d00875f30a40b3
Almy <almyventura1908@gmail.com>
2026-03-08 15:06:55 -0400

    module4-assingment5-solution

---
+++ module5-solution/js/script.js
@@ -0,0 +1,235 @@
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+});
+
+(function (global) {
+
+  var dc = {};
+
+  var homeHtmlUrl = "snippets/home-snippet.html";
+  var allCategoriesUrl =
+      "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
+  var categoriesTitleHtml = "snippets/categories-title-snippet.html";  var categoryHtml = "snippets/category-snippet.html";
+  var menuItemsUrl =
+      "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
+  var menuItemsTitleHtml = "snippets/menu-items-title.html";
+  var menuItemHtml = "snippets/menu-item.html";
+
+// Convenience function for inserting innerHTML
+  var insertHtml = function (selector, html) {
+    var targetElem = document.querySelector(selector);
+    targetElem.innerHTML = html;
+  };
+
+// Show loading icon
+  var showLoading = function (selector) {
+    var html = "<div class='text-center'>";
+    html += "<img src='images/ajax-loader.gif'></div>";
+    insertHtml(selector, html);
+  };
+
+// Substitute {{propName}} with propValue in string
+  var insertProperty = function (string, propName, propValue) {
+    var propToReplace = "{{" + propName + "}}";
+    string = string.replace(new RegExp(propToReplace, "g"), propValue);
+    return string;
+  };
+
+// Remove 'active' from home, add to menu
+  var switchMenuToActive = function () {
+    var classes = document.querySelector("#navHomeButton").className;
+    classes = classes.replace(new RegExp("active", "g"), "");
+    document.querySelector("#navHomeButton").className = classes;
+
+    classes = document.querySelector("#navMenuButton").className;
+    if (classes.indexOf("active") === -1) {
+      classes += " active";
+      document.querySelector("#navMenuButton").className = classes;
+    }
+  };
+
+// On page load
+  document.addEventListener("DOMContentLoaded", function (event) {
+
+    // STEP 0 & 1: show home view with random category
+    showLoading("#main-content");
+    $ajaxUtils.sendGetRequest(
+        allCategoriesUrl,
+        buildAndShowHomeHTML, // STEP 1: function called when server responds
+        true
+    );
+  });
+
+// Build home page HTML with categories
+  function buildAndShowHomeHTML (categories) {
+
+    // Load home snippet page
+    $ajaxUtils.sendGetRequest(
+        homeHtmlUrl,
+        function (homeHtml) {
+
+          // STEP 2: pick a random category
+          var chosenCategory = chooseRandomCategory(categories);
+          var chosenCategoryShortName = "'" + chosenCategory.short_name + "'"; // STEP 3: wrap in quotes
+
+          // STEP 3: insert random category into snippet
+          var homeHtmlToInsertIntoMainPage =
+              insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+
+          // STEP 4: insert produced HTML into main page
+          insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+
+        },
+        false
+    );
+  }
+
+// Returns a random category object
+  function chooseRandomCategory (categories) {
+    var randomArrayIndex = Math.floor(Math.random() * categories.length);
+    return categories[randomArrayIndex];
+  }
+
+// Load menu categories view
+  dc.loadMenuCategories = function () {
+    showLoading("#main-content");
+    $ajaxUtils.sendGetRequest(
+        allCategoriesUrl,
+        buildAndShowCategoriesHTML
+    );
+  };
+
+// Load menu items view
+  dc.loadMenuItems = function (categoryShort) {
+    showLoading("#main-content");
+    $ajaxUtils.sendGetRequest(
+        menuItemsUrl + categoryShort + ".json",
+        buildAndShowMenuItemsHTML
+    );
+  };
+
+// Build categories page HTML
+  function buildAndShowCategoriesHTML (categories) {
+    $ajaxUtils.sendGetRequest(
+        categoriesTitleHtml,
+        function (categoriesTitleHtml) {
+          $ajaxUtils.sendGetRequest(
+              categoryHtml,
+              function (categoryHtml) {
+                switchMenuToActive();
+                var categoriesViewHtml =
+                    buildCategoriesViewHtml(categories,
+                        categoriesTitleHtml,
+                        categoryHtml);
+                insertHtml("#main-content", categoriesViewHtml);
+              },
+              false
+          );
+        },
+        false
+    );
+  }
+
+// Build categories view HTML
+  function buildCategoriesViewHtml(categories,
+                                   categoriesTitleHtml,
+                                   categoryHtml) {
+    var finalHtml = categoriesTitleHtml;
+    finalHtml += "<section class='row'>";
+
+    for (var i = 0; i < categories.length; i++) {
+      var html = categoryHtml;
+      var name = "" + categories[i].name;
+      var short_name = categories[i].short_name;
+      html = insertProperty(html, "name", name);
+      html = insertProperty(html, "short_name", short_name);
+      finalHtml += html;
+    }
+
+    finalHtml += "</section>";
+    return finalHtml;
+  }
+
+// Build menu items page HTML
+  function buildAndShowMenuItemsHTML (categoryMenuItems) {
+    $ajaxUtils.sendGetRequest(
+        menuItemsTitleHtml,
+        function (menuItemsTitleHtml) {
+          $ajaxUtils.sendGetRequest(
+              menuItemHtml,
+              function (menuItemHtml) {
+                switchMenuToActive();
+                var menuItemsViewHtml =
+                    buildMenuItemsViewHtml(categoryMenuItems,
+                        menuItemsTitleHtml,
+                        menuItemHtml);
+                insertHtml("#main-content", menuItemsViewHtml);
+              },
+              false
+          );
+        },
+        false
+    );
+  }
+
+// Build menu items view HTML
+  function buildMenuItemsViewHtml(categoryMenuItems,
+                                  menuItemsTitleHtml,
+                                  menuItemHtml) {
+
+    menuItemsTitleHtml =
+        insertProperty(menuItemsTitleHtml, "name", categoryMenuItems.category.name);
+    menuItemsTitleHtml =
+        insertProperty(menuItemsTitleHtml, "special_instructions", categoryMenuItems.category.special_instructions);
+
+    var finalHtml = menuItemsTitleHtml;
+    finalHtml += "<section class='row'>";
+
+    var menuItems = categoryMenuItems.menu_items;
+    var catShortName = categoryMenuItems.category.short_name;
+    for (var i = 0; i < menuItems.length; i++) {
+      var html = menuItemHtml;
+      html = insertProperty(html, "short_name", menuItems[i].short_name);
+      html = insertProperty(html, "catShortName", catShortName);
+      html = insertItemPrice(html, "price_small", menuItems[i].price_small);
+      html = insertItemPortionName(html, "small_portion_name", menuItems[i].small_portion_name);
+      html = insertItemPrice(html, "price_large", menuItems[i].price_large);
+      html = insertItemPortionName(html, "large_portion_name", menuItems[i].large_portion_name);
+      html = insertProperty(html, "name", menuItems[i].name);
+      html = insertProperty(html, "description", menuItems[i].description);
+
+      if (i % 2 !== 0) {
+        html += "<div class='clearfix visible-lg-block visible-md-block'></div>";
+      }
+
+      finalHtml += html;
+    }
+
+    finalHtml += "</section>";
+    return finalHtml;
+  }
+
+// Append price
+  function insertItemPrice(html, pricePropName, priceValue) {
+    if (!priceValue) return insertProperty(html, pricePropName, "");
+    priceValue = "$" + priceValue.toFixed(2);
+    html = insertProperty(html, pricePropName, priceValue);
+    return html;
+  }
+
+// Append portion name
+  function insertItemPortionName(html, portionPropName, portionValue) {
+    if (!portionValue) return html = insertProperty(html, portionPropName, "");
+    portionValue = "(" + portionValue + ")";
+    html = insertProperty(html, portionPropName, portionValue);
+    return html;
+  }
+
+  global.$dc = dc;
+
+})(window);
